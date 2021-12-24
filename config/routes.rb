db25231ad1cfa55151f9d511b501ece6aebcf1cc
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2021-12-24 10:01:57 +0100

    projects: only show, add projects anchor to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
-  resources :projects
+  resources :projects, only: :show
 
   root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
