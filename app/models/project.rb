8dfe3f5c81d394001119509822107542d5afafce
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-12-28 12:10:02 +0100

    missing video check & one more video added

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
+
+  default_scope {order(:name)}
   
   def big_image
     self.image.gsub('.png','-big.png')
