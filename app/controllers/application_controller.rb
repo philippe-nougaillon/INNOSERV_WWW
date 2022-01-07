3c335bf5907db2523f6c563382461b3d920bbf18
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-01-07 12:47:33 +0100

    bug fixed / design improved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :detect_device_format
+  
+  private
+
+    def detect_device_format
+      case request.user_agent
+      when /iPhone/i, /Android/i && /mobile/i, /Windows Phone/i
+          request.variant = :phone
+      else
+          request.variant = :desktop
+      end
+    end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,8 @@
 class ContactMailer < ApplicationMailer
   def submitted(email, subject, content)
     @email = email
+    @subject = subject
+    @content = content
     mail to: "philippe.nougaillon@gmail.com", subject: "INNOSERV New Contact"
   end
 end
