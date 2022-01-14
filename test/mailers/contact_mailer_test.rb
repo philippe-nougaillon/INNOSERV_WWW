ec2fc44fbdb24fe18d07556d6b15dc4403e1ad47
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-01-14 16:10:41 +0100

    mailer test

--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class ContactMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "submitted" do
+    mail = ContactMailer.submitted("test@gmail.com","MySubject","MyContent")
+    assert_equal "INNOSERV New Contact", mail.subject
+    assert_equal ["philippe.nougaillon@gmail.com"], mail.to
+    assert_equal ["contact@innoserv-project.eu"], mail.from
+  end
 end
