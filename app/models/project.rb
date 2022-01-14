a5c8f224afd7546c331bbd8819e988c0cf4c3702
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-01-14 14:24:39 +0100

    project tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,8 @@ class Project < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
+  validates :name, :description, :image, :information, :videofile, :fieldOfEducation, :fieldOfWelfare, :fieldOfHealth, :video, presence: true
+
   default_scope {order(:name)}
   
 end
